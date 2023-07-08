7eefa2adfa094bf7c2366f1238a3f22376895639
Engine <rg.dartigue@gmail.com>
2023-07-07 22:09:34 -0400

    Began work in JS file. For the time being, place holder text was added to test out the buttons, the topic will change in later updates. Click fuction has been added along with the loop to check if selected answer is correct or incorrect. Updated CSS to change the back ground color to red if false or green if true.

---
+++ Assets/js/script.js
@@ -0,0 +1,106 @@
+// ======================= QUESTIONS==========================================================//
+const questions = [
+    //index 0 quest 1
+    {
+        question: "What is the second planet in the solar system?",
+        answers: [
+            { text: "Sedna", correct: false},
+            { text: "Venus", correct: true},
+            { text: "Earth", correct: false},
+            { text: "Pluto", correct: false},
+        ]
+    },
+    //index 1 quest 2
+    {
+        question: "What is the second planet in the solar system again?",
+        answers: [
+            { text: "Venus", correct: true},
+            { text: "Sedna", correct: false},
+            { text: "Earth", correct: false},
+            { text: "Pluto", correct: false},
+        ]
+    },
+    //index 2 quest 3
+    {
+        question: "Sorry, I missed that. What is the second planet in the solar system",
+        answers: [
+            { text: "Sedna", correct: false},
+            { text: "Earth", correct: false},
+            { text: "Venus", correct: true},
+            { text: "Pluto", correct: false},
+        ]
+    },
+    //index 3 quest 4
+    {
+        question: "One more time. What is the second planet in the solar system",
+        answers: [
+            { text: "Sedna", correct: false},
+            { text: "Pluto", correct: false},
+            { text: "Earth", correct: false},
+            { text: "Venus", correct: true},
+        ]
+    }
+];
+
+// ===================================BUTTONS================================================================================//
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const  nextButton = document.getElementById("next-btn");
+
+//question start at 1
+let currentQuestionIndex = 0;
+let score = 0;
+
+//when quiz starts, question will be reset to 0
+//score will be set to 0
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+//displays the next question
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+//displays the answers
+        //create a button tag and will save it as a variable "button"
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+    //adds the true or false in the dataset.
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+//dataset will be checked if user has selected a true or false answer.
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    } else{
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
